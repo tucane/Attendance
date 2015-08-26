946d5e8c8d91617e280994b68c9db66e49504501
Bill Gan <BillyG@Bills-MacBook-Pro.local>
2015-08-25 21:45:00 -0400

    Initial Commit

---
+++ Attendance-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Attendance-App
+//
+//  Created by Bill Gan on 2015-08-25.
+//  Copyright (c) 2015 InsightSearch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
